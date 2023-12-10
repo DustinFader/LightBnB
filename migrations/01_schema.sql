ae7c728d5dbf2602effa31053d4b5dd161b07b7d
DustinFader <dustinfader16@gmail.com>
2023-12-10 15:01:11 -0400

    add large seed file, delete temp comments, rename a table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,15 +1,15 @@
-DROP TABLE IF EXISTS "users", "properties", "reservations", "propertie_reviews" CASCADE;
+DROP TABLE IF EXISTS "users", "properties", "reservations", "property_reviews" CASCADE;
 
 CREATE TABLE users (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" VARCHAR(255) NOT NULL,
-  "email_address" VARCHAR(255) NOT NULL,
+  "email" VARCHAR(255) NOT NULL,
   "password" VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties (
   "id" SERIAL PRIMARY KEY NOT NULL,
-  "owner_id" INTEGER,
+  "owner_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
   "title" VARCHAR(255) NOT NULL,
   "description" TEXT,
@@ -18,8 +18,8 @@ CREATE TABLE properties (
   "number_of_bathrooms" INTEGER NOT NULL DEFAULT 0,
   "number_of_bedrooms" INTEGER NOT NULL DEFAULT 0,
   
-  "thumbnail_photo" VARCHAR(255) NOT NULL,
-  "cover_photo" VARCHAR(255) NOT NULL,
+  "thumbnail_photo_url" VARCHAR(255) NOT NULL,
+  "cover_photo_url" VARCHAR(255) NOT NULL,
   "country" VARCHAR(255) NOT NULL,
   "street" VARCHAR(255) NOT NULL,
   "city" VARCHAR(255) NOT NULL,
@@ -32,23 +32,23 @@ CREATE TABLE reservations (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "start_date" DATE NOT NULL,
   "end_date" DATE NOT NULL,
-  "guest_id" INTEGER,
-  "property_id" INTEGER
+  "guest_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "property_id" INTEGER REFERENCES properties(id) ON DELETE CASCADE
 );
 
-CREATE TABLE propertie_reviews (
+CREATE TABLE property_reviews (
   "id" SERIAL PRIMARY KEY NOT NULL,
-  "guest_id" INTEGER,
-  "property_id" INTEGER,
-  "reservation_id" INTEGER,
+  "guest_id" INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  "property_id" INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  "reservation_id" INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   "rating" SMALLINT NOT NULL DEFAULT 0,
   "message" TEXT
 );
 
-ALTER TABLE "properties" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+-- ALTER TABLE "properties" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "reservations" ADD FOREIGN KEY ("guest_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+-- ALTER TABLE "reservations" ADD FOREIGN KEY ("guest_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "propertie_reviews" ADD FOREIGN KEY ("guest_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+-- ALTER TABLE "property_reviews" ADD FOREIGN KEY ("guest_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
-ALTER TABLE "reservations" ADD FOREIGN KEY ("property_id") REFERENCES "properties" ("id") ON DELETE CASCADE;
+-- ALTER TABLE "reservations" ADD FOREIGN KEY ("property_id") REFERENCES "properties" ("id") ON DELETE CASCADE;
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -8,12 +8,12 @@ VALUES (1, 1, 'openses', 'description', 'https://images.pexels.com/photos/208667
 (2, 2, 'loper', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 20, 2, 1, 1, 'canada', 'route', 'city', 'alberta', '5rl po7', true),
 (3, 3, 'gonker', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 23, 2, 1, 1, 'canada', 'route', 'city', 'alberta', 'q5l p62', true);
 
-INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2390-09-23', '2391-08-12', 1, 1),
-('2200-09-23', '2201-08-12', 2, 2),
-('1400-09-23', '1401-08-12', 3, 3);
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+VALUES (1, '2390-09-23', '2391-08-12', 1, 1),
+(2, '2200-09-23', '2201-08-12', 2, 2),
+(3, '1400-09-23', '1401-08-12', 3, 3);
 
-INSERT INTO propertie_reviews (id, guest_id, property_id, reservation_id, rating, message)
+INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
 VALUES (1, 3, 1, 1, 3, 'message'),
 (2, 3, 2, 2, 2, 'message'),
 (3, 3, 1, 3, 0, 'message');
